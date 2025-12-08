0956077fce8402ff34b8396542a514ec42546bda
SylMeu13 <sylvain.meunier.pro@gmail.com>
2025-12-08 17:08:56 +0100

    rename DashboardRoutes function

--- src/router/middleswares/DashboardRoutes.jsx
+++ src/router/middleswares/DashboardRoutes.jsx
@@ -1,7 +1,7 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { getUserRole } from "../../utils/storage";
 
-export default function PrivateRoutes({ role, redirect = "/" }) {
+export default function DashboardRoutes({ role, redirect = "/" }) {
   const userRole = getUserRole();
   return userRole == role ? <Outlet /> : <Navigate to={redirect} />;
 }
