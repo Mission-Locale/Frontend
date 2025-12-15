f0da28cc5e9c8818581cf91c37c7f06ffdc7341f
SylMeu13 <sylvain.meunier.pro@gmail.com>
2025-12-15 18:05:49 +0100

    fix styling

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -1,27 +1,28 @@
 import { createPortal } from "react-dom";
 import useMobileToggle from "../hooks/useMobileToggle";
 
-export default function Modal({ key, childrens, onOutOfBoundClick }) {
+export default function Modal({ modalKey, children, onOutOfBoundClick }) {
   const isMobile = useMobileToggle();
   return createPortal(
     <div
-      className={`fixed bg-black opacity-50 w-screen h-screen flex flex-col ${
-        isMobile ? "justify-end" : "justify-center"
-      } justify-items-center`}
+      className={`fixed bg-black/50 w-screen h-screen flex flex-col items-center overflow-y-auto`}
       onClick={onOutOfBoundClick}
     >
+      {/* Used margins instead of justify-content as a workaround for a known scrolling issues */}
       <div
         role="dialog"
         aria-modal
         className={`${
-          isMobile ? "rounded-t-lg" : "rounded-lg"
-        } bg-white border-lightBorder w-screen flex flex-col`}
+          isMobile
+            ? "rounded-t-2xl w-screen mt-auto"
+            : "rounded-2xl w-fit my-auto"
+        } p-4 bg-white border-lightBorder flex flex-col gap-2`}
         onClick={(e) => e.stopPropagation()}
       >
-        {childrens}
+        {children}
       </div>
     </div>,
     document.body,
-    key
+    modalKey
   );
 }
